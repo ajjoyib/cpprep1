4e2cdf766cefd2f6b1aa966a636368a21c223d6e
Hamidullo <113084184+ajjoyib@users.noreply.github.com>
2023-03-07 09:42:38 +0900

    Delete main.cpp

--- main.cpp
+++
@@ -1,6 +0,0 @@
-#include <cstdio>
-
-int main() { //entry point - a function that executes when a user runs a program
-	printf("Hello World");
-	return 0; // exit code - an integer value that the opeating system uses to determine how well a program ran
-}
